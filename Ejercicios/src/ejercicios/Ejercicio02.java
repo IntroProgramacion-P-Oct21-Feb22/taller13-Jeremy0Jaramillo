5565744144a18f4b3972edb2a25a34e812cc6d8e
Jeremy <jfjaramillo19@utpl.edu.ec>
2022-01-30 09:31:40 -0500

    sss

--- Ejercicios/src/ejercicios/Ejercicio02.java
+++ Ejercicios/src/ejercicios/Ejercicio02.java
@@ -5,6 +5,7 @@
  */
 package ejercicios;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,20 +16,31 @@ public class Ejercicio02 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double calificacion;
-        double promedio;
-        double suma = 0;
-        int numeroCalificaciones = 4;
-        int i = 1;
-        while(i<=4){
-            System.out.println("Ingrese calificación: \n");
-            calificacion = entrada.nextDouble();
-            suma = suma + calificacion;
-            i = i + 1;
+        try {
+            double calificacion;
+            double promedio;
+            double suma = 0;
+            int numeroCalificaciones = 4;
+            int i = 1;
+            while (i <= 4) {
+                System.out.println("Ingrese calificación: \n");
+                calificacion = entrada.nextDouble();
+                if (calificacion > 10) {
+                    throw new InputMismatchException("El valor ingresado es mayor a 10");
+                } else {
+                    if (calificacion < 0) {
+                        throw new InputMismatchException("El valor ingresado es menor a 0");
+                    }
+                }
+                suma = suma + calificacion;
+                i = i + 1;
+            }
+
+            promedio = suma / numeroCalificaciones;
+
+            System.out.printf("%.2f\n", promedio);
+        } catch (InputMismatchException e) {
+            System.out.printf("(InputMismatchException) Ocurrió una excepción %s\n", e);
         }
-
-        promedio = suma / numeroCalificaciones;
-
-        System.out.printf("%.2f\n", promedio);
     }
 }
--- Ejercicios/src/ejercicios/Ejercicio04.java
+++ Ejercicios/src/ejercicios/Ejercicio04.java
@@ -5,6 +5,7 @@
  */
 package ejercicios;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,6 +16,7 @@ public class Ejercicio04 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        try{
         double sueldo;
         String nombre;
         double[] arregloSueldos;
@@ -28,12 +30,18 @@ public class Ejercicio04 {
             
             System.out.println("Cuantos sueldos quiere agregar al arreglo");
             numeroDatosArreglo = entrada.nextInt();
+            if(numeroDatosArreglo > 5 || numeroDatosArreglo < 0){
+            throw new Exception("El valor ingresado se encuentra fuera de los límites (0-5)");
+            }
             arregloSueldos = new double[numeroDatosArreglo];
             j = 0;
             while(j < arregloSueldos.length){
                 System.out.printf("agregue sueldo %d para usuario %d\n",
                         j, i);
                 sueldo = entrada.nextDouble();
+                if(sueldo >10 || sueldo < 0){
+                throw new InputMismatchException("El valor ingresado se encuentra fuera de los límites (0-10)");
+                }
                 arregloSueldos[j] = sueldo;
                 j = j + 1;
             }
@@ -47,5 +55,10 @@ public class Ejercicio04 {
         }
 
         System.out.printf("%s\n", "Gracias por usar el sistema");
+        }catch (InputMismatchException e) {
+            System.out.printf("(InputMismatchException) Ocurrió una excepción %s\n", e);
+        }catch (Exception e) {
+            System.out.printf("Ocurrió una excepción %s\n", e);
+        }
     }
 }
